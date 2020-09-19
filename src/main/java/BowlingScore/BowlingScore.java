b17ea20461c76569878476fd70da08ee4874a037
dragonwang-hub <915614879@qq.com>
2020-09-20 00:47:03 +0800

    [dragon] feat: should_throw_exception_when_not_finish_competition

---
+++ src/main/java/BowlingScore/BowlingScore.java
@@ -0,0 +1,30 @@
+package BowlingScore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BowlingScore {
+    public int calculationScore(int[][] eachTimeScore) {
+        if (eachTimeScore == null || eachTimeScore.length < 10) {
+            //throw exception
+        }
+        List<Integer> eachFrameScore = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int iTimesFrameCurScore = eachTimeScore[i][0] + eachTimeScore[i][1];
+            if (eachTimeScore[i][0] == 10) {
+                if (eachTimeScore[i + 1][0] == 10) {
+                    iTimesFrameCurScore = iTimesFrameCurScore + eachTimeScore[i + 1][0] + eachTimeScore[i + 2][0];
+                } else {
+                    iTimesFrameCurScore = iTimesFrameCurScore + eachTimeScore[i + 1][0] + eachTimeScore[i + 1][1];
+                }
+            } else if (iTimesFrameCurScore == 10) {
+                iTimesFrameCurScore = iTimesFrameCurScore + eachTimeScore[i + 1][0];
+            }
+            eachFrameScore.add(iTimesFrameCurScore);
+        }
+        //int totalScore = 0;
+        int totalScore = eachFrameScore.stream().reduce(0, Integer::sum);
+        return totalScore;
+
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,15 @@
+import BowlingScore.BowlingScore;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class BowlingGameTest {
+    @Test
+    void should_throw_exception_when_not_finish_competition() {
+        assertThrows(Exception.class, () -> {
+            BowlingScore bowlingScore = new BowlingScore();
+            int[][] eachTimeScoreOfNotFinishCompetition = {{1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}};
+            bowlingScore.calculationScore(eachTimeScoreOfNotFinishCompetition);
+        });
+    }
 }
